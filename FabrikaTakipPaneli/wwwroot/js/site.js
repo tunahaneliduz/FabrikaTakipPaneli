84fef4fd781f2cbb54a841575e30ff7d23235adb
tunahaneliduz <tunahaneliduz@trakya.edu.tr>
2026-08-13 09:33:25 +0300

    Arayüz teması, ikon ve font güncellendi

--- FabrikaTakipPaneli/wwwroot/js/site.js
+++ FabrikaTakipPaneli/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+(function () {
+    var toggle = document.getElementById('themeToggle');
+    if (!toggle) {
+        return;
+    }
+
+    toggle.addEventListener('click', function () {
+        var current = document.documentElement.getAttribute('data-bs-theme');
+        var next = current === 'dark' ? 'light' : 'dark';
+        document.documentElement.setAttribute('data-bs-theme', next);
+        localStorage.setItem('theme', next);
+    });
+})();
